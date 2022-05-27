efc81216986ad1e177c3a6c5262815ce6ea8988c
mohsen madi <mohsen@backbase.com>
2022-05-27 12:37:45 -0400

    comps added

--- apps/ng-contacts/src/app/app.module.ts
+++ apps/ng-contacts/src/app/app.module.ts
@@ -3,9 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { AddContactComponent } from './comps/add-contact/add-contact.component';
+import { ContactsComponent } from './comps/contacts/contacts.component';
+import { HeaderComponent } from './comps/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    AddContactComponent,
+    ContactsComponent,
+    HeaderComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ apps/ng-contacts/src/app/comps/add-contact/add-contact.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'comp-store-add-contact',
+  templateUrl: './add-contact.component.html',
+  styleUrls: ['./add-contact.component.scss'],
+})
+export class AddContactComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

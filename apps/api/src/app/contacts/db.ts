8766f5892ae63465064a55f7c976ef4de44edc8a
mohsen madi <mohsen@backbase.com>
2022-06-10 11:09:34 -0400

    fixed db

--- apps/api/src/app/contacts/db.ts
+++ apps/api/src/app/contacts/db.ts
@@ -3,56 +3,44 @@ import { Contact } from "@comp-store/data-model";
 export const CONTACTS: Contact[] = [
   {
     id: '10',
-    name: 'Bibbye Gutcher',
-    phone: '885-131-9176',
-    email: 'bgutcher0@smh.com.au'
+    name: 'Diego Global',
+    phone: '111-222-3333',
+    email: 'diego@global.com'
   },
   {
     id: '20',
-    name: 'Gabbie Haslegrave',
-    phone: '541-172-6277',
-    email: 'ghaslegrave1@mashable.com'
+    name: 'Gavin East',
+    phone: '222-444-3212',
+    email: 'gavin@east.com'
   },
   {
     id: '30',
-    name: 'Catie Nettleship',
-    phone: '201-539-6873',
-    email: 'cnettleship2@google.nl'
+    name: 'Manoj Stillaway',
+    phone: '123-432-4567',
+    email: 'manoj@away.in'
   },
   {
     id: '40',
-    name: 'Mahalia Sones',
-    phone: '535-669-7499',
-    email: 'msones3@ihg.com'
+    name: 'Terry Scorer',
+    phone: '453-444-6666',
+    email: 'terry@game.com'
   },
   {
     id: '50',
-    name: 'Lia Yurmanovev',
-    phone: '381-531-3649',
-    email: 'lyurmanovev4@bravesites.com'
+    name: 'Dhairya Dirtycsser',
+    phone: '456-777-6666',
+    email: 'dhairya@css.com'
   },
   {
     id: '60',
-    name: 'Daisy Simonel',
+    name: 'Mohsen Oshawa',
     phone: '890-576-1438',
-    email: 'dsimonel5@php.net'
+    email: 'mohsen@east.ca'
   },
   {
     id: '70',
-    name: 'Bobby Coller',
-    phone: '129-638-2410',
-    email: 'bcoller6@usda.gov'
-  },
-  {
-    id: '80',
-    name: 'Peri Hockey',
-    phone: '290-234-4247',
-    email: 'phockey7@edublogs.org'
-  },
-  {
-    id: '90',
-    name: 'Cordie Crosscombe',
-    phone: '907-576-1311',
-    email: 'ccrosscombe8@youku.com'
+    name: 'Kern Thekid',
+    phone: '555-123-3454',
+    email: 'kern@bb.com'
   }
 ];
